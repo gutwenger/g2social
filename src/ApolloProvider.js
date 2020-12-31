14e06cd828de02028e71a32fbc2b5ac478d76000
Fremont Cheng <fremont@Fremont.local>
2020-12-31 16:36:15 +0800

    first commit g2social

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
     // uri: "http://localhost:5000"
-    uri: "https://g2social-backend.herokuapp.com/"
+    uri: "https://g2social-backend.herokuapp.com"
 })
 
 const authLink = setContext(() => {
